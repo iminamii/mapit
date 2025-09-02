86d4aaaf035a851e299112bcaa5615196c3db5cd
iminamii <iminamii@gmail.com>
2025-09-02 20:45:34 +0900

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,7 +3,7 @@
 # 環境変数からAPIキーを読み込む
 API_KEY=$GOOGLE_MAPS_API_KEY
 
-pwd
-ls
+mkdir -p dist
+
 # テンプレートファイルを読み込み、APIキーを置き換えて新しいHTMLファイルを作成する
 sed "s/{{GOOGLE_MAPS_API_KEY}}/$API_KEY/g" ./map.html.template > ./dist/map.html
